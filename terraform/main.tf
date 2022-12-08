2922b3a0c30afb04ae59f192caac4142ad4ba3f5
Jaye <jshin0219@gmail.com>
2022-12-08 15:30:11 +0900

    modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,43 @@ provider "google" {
   region = "us-west1"
 }
 
-resource "google_compute_instance" "test" {
-  name         = "test"
+resource "google_compute_firewall" "allow-firewall" {
+  name    = "allow-firewall"
+  network = "default"
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["firewall"]
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "8080", "3389"]
+  }
+}
+
+resource "google_compute_instance" "myinstance" {
+  name         = "myinstance"
   machine_type = "e2-micro"
-  zone         = "us-west1-a"
+  zone         = "us-west1-c"
+
+  allow_stopping_for_update = true
+
+  tags = ["firewall"]
 
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-1804-bionic-v20221201"
-      labels = {
-        my_label = "value"
-      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
     }
   }
 }
